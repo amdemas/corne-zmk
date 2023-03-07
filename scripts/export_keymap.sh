babcb9e767d5a3c2fe0924635c3f0dfd906aa38e
Andrew Demas <amdemas@users.noreply.github.com>
2023-03-06 21:31:01 -0500

    Update to include more combos for missing keys

--- scripts/export_keymap.sh
+++ scripts/export_keymap.sh
@@ -3,7 +3,7 @@
 # Export keymap using keymap-drawer
 # (https://github.com/caksoylar/keymap-drawer)
 
-
+rm corne_keymap.svg
 keymap parse -c 10 -z ../config/corne.keymap > corne_keymap.yaml
-keymap draw -k crkbd/rev1 corne_keymap.yaml > corne_keymap.svg
+keymap draw -k crkbd/rev1 -l LAYOUT_split_3x5_3 corne_keymap.yaml > corne_keymap.svg
 rm corne_keymap.yaml
