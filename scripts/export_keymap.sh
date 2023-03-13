a79036fba87d7527356f6d0161b0539fa5cab1f6
Andrew Demas <amdemas@users.noreply.github.com>
2023-03-13 16:27:12 -0400

    Update keymap export

--- scripts/export_keymap.sh
+++ scripts/export_keymap.sh
@@ -4,6 +4,6 @@
 # (https://github.com/caksoylar/keymap-drawer)
 
 rm corne_keymap.svg
-keymap parse -c 10 -z ../config/corne.keymap > corne_keymap.yaml
-keymap draw -k crkbd/rev1 -l LAYOUT_split_3x5_3 corne_keymap.yaml > corne_keymap.svg
+keymap -c config.yaml parse -c 10 -z ../config/corne.keymap > corne_keymap.yaml
+keymap -c config.yaml draw -k crkbd/rev1 -l LAYOUT_split_3x5_3 corne_keymap.yaml > corne_keymap.svg
 rm corne_keymap.yaml
